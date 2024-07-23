3b88ab22d41c3380939c32e5d6cf691555eda5ab
Jeff Honsowetz <jeffh@gointerject.com>
2024-07-22 23:25:24 -0700

    further setup lane

--- DB_Scripting.ps1
+++ DB_Scripting.ps1
@@ -5,11 +5,11 @@
 #### TEST
 # AppDatabase
 powershell.exe -File "C:\Users\jeffh\OneDrive\Documents\GitRepos\devlane_database_example\GenerateSqlScripts_v2.ps1" "." "AppDatabase_Test" "AppDatabase" "C:\Users\jeffh\OneDrive\Documents\GitRepos\devlane_database_example\DB" true false true
-# ReportingDatabase
-powershell.exe -File "C:\Users\jeffh\OneDrive\Documents\GitRepos\devlane_database_example\GenerateSqlScripts_v2.ps1" "." "ReportingDatabase_Test" "ReportingDatabase" "C:\Users\jeffh\OneDrive\Documents\GitRepos\devlane_database_example\DB" true false true
+# AppReportingDatabase
+powershell.exe -File "C:\Users\jeffh\OneDrive\Documents\GitRepos\devlane_database_example\GenerateSqlScripts_v2.ps1" "." "AppReportingDatabase_Test" "AppReportingDatabase" "C:\Users\jeffh\OneDrive\Documents\GitRepos\devlane_database_example\DB" true false true
 
 #### PROD
 # AppDatabase
 powershell.exe -File "C:\Users\jeffh\OneDrive\Documents\GitRepos\devlane_database_example\GenerateSqlScripts_v2.ps1" "." "AppDatabase_Prod" "AppDatabase" "C:\Users\jeffh\OneDrive\Documents\GitRepos\devlane_database_example\DB" true false true
-# ReportingDatabase
-powershell.exe -File "C:\Users\jeffh\OneDrive\Documents\GitRepos\devlane_database_example\GenerateSqlScripts_v2.ps1" "." "ReportingDatabase_Prod" "ReportingDatabase" "C:\Users\jeffh\OneDrive\Documents\GitRepos\devlane_database_example\DB" true false true
+# AppReportingDatabase
+powershell.exe -File "C:\Users\jeffh\OneDrive\Documents\GitRepos\devlane_database_example\GenerateSqlScripts_v2.ps1" "." "AppReportingDatabase_Prod" "AppReportingDatabase" "C:\Users\jeffh\OneDrive\Documents\GitRepos\devlane_database_example\DB" true false true
