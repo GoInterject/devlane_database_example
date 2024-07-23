241ab40f1c2098581167a28a26ea43b71d212bdd
Jeff Honsowetz <jeffh@gointerject.com>
2024-07-22 22:50:12 -0700

    update script

--- DB_Scripting.ps1
+++ DB_Scripting.ps1
@@ -6,7 +6,7 @@
 # AppDatabase
 powershell.exe -File "C:\Users\jeffh\OneDrive\Documents\GitRepos\devlane_database_example\GenerateSqlScripts_v2.ps1" "." "AppDatabase_Test" "AppDatabase" "C:\Users\jeffh\OneDrive\Documents\GitRepos\devlane_database_example\DB" true false true
 # ReportingDatabase
-powershell.exe -File "C:\Users\jeffh\OneDrive\Documents\GitRepos\devlane_database_example\GenerateSqlScripts_v2.ps1" "." "ReportingDatabase_Test" "Interject_Reporting" "C:\Users\jeffh\OneDrive\Documents\GitRepos\devlane_database_example\DB" true false true
+powershell.exe -File "C:\Users\jeffh\OneDrive\Documents\GitRepos\devlane_database_example\GenerateSqlScripts_v2.ps1" "." "ReportingDatabase_Test" "ReportingDatabase" "C:\Users\jeffh\OneDrive\Documents\GitRepos\devlane_database_example\DB" true false true
 
 #### PROD
 # AppDatabase
