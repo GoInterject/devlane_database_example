f0b6a4ec31a9771bd3a9be750b7583155c4644b9
Jeff Honsowetz <jeffh@gointerject.com>
2024-07-22 22:42:17 -0700

    Initial commit for Test DB

---
+++ DB_Scripting.ps1
@@ -0,0 +1,15 @@
+##
+##  to script into a GitRepos repo folder for version review
+##
+
+#### TEST
+# AppDatabase
+powershell.exe -File "C:\Users\jeffh\OneDrive\Documents\GitRepos\devlane_database_example\GenerateSqlScripts_v2.ps1" "." "AppDatabase_Test" "AppDatabase" "C:\Users\jeffh\OneDrive\Documents\GitRepos\devlane_database_example\DB" true false true
+# ReportingDatabase
+powershell.exe -File "C:\Users\jeffh\OneDrive\Documents\GitRepos\devlane_database_example\GenerateSqlScripts_v2.ps1" "." "ReportingDatabase_Test" "Interject_Reporting" "C:\Users\jeffh\OneDrive\Documents\GitRepos\devlane_database_example\DB" true false true
+
+#### PROD
+# AppDatabase
+powershell.exe -File "C:\Users\jeffh\OneDrive\Documents\GitRepos\devlane_database_example\GenerateSqlScripts_v2.ps1" "." "AppDatabase_Prod" "AppDatabase" "C:\Users\jeffh\OneDrive\Documents\GitRepos\devlane_database_example\DB" true false true
+# ReportingDatabase
+powershell.exe -File "C:\Users\jeffh\OneDrive\Documents\GitRepos\devlane_database_example\GenerateSqlScripts_v2.ps1" "." "ReportingDatabase_Prod" "ReportingDatabase" "C:\Users\jeffh\OneDrive\Documents\GitRepos\devlane_database_example\DB" true false true
